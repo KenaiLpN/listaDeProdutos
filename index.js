45ff0288eaa2a657ad5380e906321aaa14006e11
Leonardo Kenai Nascimento <leonardopivanascimento@gmail.com>
2025-02-26 18:09:04 -0300

    PrimeiroUpdate

---
+++ index.js
@@ -0,0 +1,77 @@
+// 'https://fakestoreapi.com/products'
+
+const apiUrl = 'https://fakestoreapi.com/products'
+let produtosGlobal = [];
+
+async function consumirProdutos() {
+    try{
+        const resposta = await fetch(apiUrl)
+        const produtos = await resposta.json()
+        produtosGlobal = produtos;
+       
+        atualizarTabela(produtosGlobal);
+    }catch (erro) {
+        console.error('Erro ao buscar produtos:', erro);
+    }}
+
+
+    function atualizarTabela(produtos){
+
+        const tbody = document.getElementById('tabelaDeProdutos')
+        tbody.innerHTML = '';
+
+
+ 
+
+        produtos.forEach(produto => {
+            const linha = document.createElement('tr')
+            linha.className = 'text-center even:bg-blue-50 h-10'
+
+            const celulaId = document.createElement('td')
+            celulaId.className = ''
+            celulaId.textContent = produto.id
+            linha.appendChild(celulaId)
+
+            const nomeProd = document.createElement('td')
+            nomeProd.className = 'font-bold'
+            nomeProd.textContent = produto.title
+            linha.appendChild(nomeProd)
+
+            const preco = document.createElement('td')    
+            preco.className = ''
+            preco.textContent = produto.price.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
+            linha.appendChild(preco)
+        
+
+            const estoque = document.createElement('td')
+            estoque.className = ''
+            estoque.textContent = produto.rating.count
+            linha.appendChild(estoque)
+            
+
+            tbody.appendChild(linha)
+            
+
+        });
+   
+    }
+
+    function filtrarProdutos(){
+        const filtro = document.getElementById("botaoPesquisa").value.toLowerCase();
+        const produtosFiltrados = produtosGlobal.filter(produto =>
+            produto.title.toLowerCase().includes(filtro)
+        );
+        atualizarTabela(produtosFiltrados);
+    }
+     
+    function filtroPreco(){
+        const precoMin = document.getElementById("precoMin").value.toString()
+        const precoMinfiltrado = produtosGlobal.filter(produto =>
+            produto.price.toString().includes(precoMin)
+        )
+
+    }
+
+
+consumirProdutos();
+
