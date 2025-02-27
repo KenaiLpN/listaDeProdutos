222371e499adb55dacb54d0246a9e7c5d047c8db
Leonardo Kenai Nascimento <leonardopivanascimento@gmail.com>
2025-02-27 09:30:55 -0300

    Update de comentários

--- index.js
+++ index.js
@@ -3,10 +3,11 @@
 const apiUrl = 'https://fakestoreapi.com/products'
 let produtosGlobal = [];
 
+// Função criada para puxar a lista de produtos e guardar na const "produtos"
 async function consumirProdutos() {
     try{
-        const resposta = await fetch(apiUrl)
-        const produtos = await resposta.json()
+        const resposta = await fetch(apiUrl) //puxa o .json de produtos da URL
+        const produtos = await resposta.json() // armazena 
         produtosGlobal = produtos;
        
         atualizarTabela(produtosGlobal);
@@ -14,7 +15,7 @@ async function consumirProdutos() {
         console.error('Erro ao buscar produtos:', erro);
     }}
 
-
+//função para criar as linhas e colunas de acordo com a lista de produtos
     function atualizarTabela(produtos){
 
         const tbody = document.getElementById('tabelaDeProdutos')
@@ -22,31 +23,31 @@ async function consumirProdutos() {
 
 
  
-
+        //cria as linhas para cada produto 
         produtos.forEach(produto => {
             const linha = document.createElement('tr')
             linha.className = 'text-center even:bg-blue-50 h-10'
 
-            const celulaId = document.createElement('td')
+            const celulaId = document.createElement('td') //cria a coluna id
             celulaId.className = ''
             celulaId.textContent = produto.id
-            linha.appendChild(celulaId)
+            linha.appendChild(celulaId)//insere a celulaId dentro da const Linha
 
-            const nomeProd = document.createElement('td')
+            const nomeProd = document.createElement('td') //cria a coluna do nome do produto
             nomeProd.className = 'font-bold'
             nomeProd.textContent = produto.title
-            linha.appendChild(nomeProd)
+            linha.appendChild(nomeProd)//insere a celula nomeProd dentro da const Linha
 
-            const preco = document.createElement('td')    
+            const preco = document.createElement('td')  //cria a coluna preço
             preco.className = ''
             preco.textContent = produto.price.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
-            linha.appendChild(preco)
+            linha.appendChild(preco)//insere a celula preco dentro da const linha
         
 
-            const estoque = document.createElement('td')
+            const estoque = document.createElement('td') //cria a coluna estoque
             estoque.className = ''
             estoque.textContent = produto.rating.count
-            linha.appendChild(estoque)
+            linha.appendChild(estoque)//insere a celula estoque dentro da const Linha
             
 
             tbody.appendChild(linha)
@@ -55,7 +56,7 @@ async function consumirProdutos() {
         });
    
     }
-
+    //função de filtro de produtos por nome
     function filtrarProdutos(){
         const filtro = document.getElementById("botaoPesquisa").value.toLowerCase();
         const produtosFiltrados = produtosGlobal.filter(produto =>
@@ -63,7 +64,7 @@ async function consumirProdutos() {
         );
         atualizarTabela(produtosFiltrados);
     }
-     
+     //função de filtro de preço Min e Mãx
     function filtroPreco(){
         const precoMin = parseFloat(document.getElementById("precoMin").value);
         const precoMax = parseFloat(document.getElementById("precoMax").value);
@@ -73,7 +74,8 @@ async function consumirProdutos() {
         )
           atualizarTabela(produtosFiltrados);
     }
-
+    
+    
 
 consumirProdutos();
 
